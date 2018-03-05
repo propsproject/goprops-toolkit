6bd1fe0e45926454188a73d018cfdec6de108794
Khalil Claybon <kclaybon1@gmail.com>
2018-03-05 12:58:59 -0500

    use custom logger with multiple encoders

--- logger/v2/logger.go
+++ logger/v2/logger.go
@@ -1,9 +1,8 @@
 package v2
 
 import (
-	"fmt"
+	"io/ioutil"
 	"os"
-	"path"
 
 	"go.uber.org/zap/zapcore"
 
@@ -17,79 +16,80 @@ const (
 
 // LoggerWrapper light convience wrapper around zap logger
 type LoggerWrapper struct {
-	zapLogger *zap.SugaredLogger
+	zapLogger *zap.Logger
 }
 
-// Field convience struct for logs with fields
+// Field convience struct for logs with fields, (support for strings only so far)
 type Field struct {
 	Key   string
-	Value interface{}
+	Value string
 }
 
 // Logger ...
 var Logger *LoggerWrapper
 
 // Info ...
-func (l *LoggerWrapper) Info(msg string, data map[string]interface{}) {
-	if args, ok := l.unpackArgs(data); ok {
-		l.zapLogger.Infof(msg, args)
-		return
-	}
-	l.zapLogger.Info(msg)
+func (l *LoggerWrapper) Info(msg string, data ...zapcore.Field) {
+	l.zapLogger.Info(msg, data...)
 }
 
 // Warn ...
-func (l *LoggerWrapper) Warn(msg string, data map[string]interface{}) {
-	if args, ok := l.unpackArgs(data); ok {
-		l.zapLogger.Warnf(msg, args)
-		return
-	}
-	l.zapLogger.Warn(msg)
+func (l *LoggerWrapper) Warn(msg string, data ...zapcore.Field) {
+	l.zapLogger.Warn(msg, data...)
 }
 
 // Error ...
-func (l *LoggerWrapper) Error(err error, data map[string]interface{}) {
-	if args, ok := l.unpackArgs(data); ok {
-		l.zapLogger.Errorf(err.Error(), args)
-		return
-	}
-	l.zapLogger.Error(err.Error())
-}
-
-func (l *LoggerWrapper) unpackArgs(args map[string]interface{}) ([]interface{}, bool) {
-	var unpacked []interface{}
-	for key, val := range args {
-		unpacked = append(unpacked, key, val)
-	}
-	return unpacked, len(unpacked) > 0
+func (l *LoggerWrapper) Error(err error, data ...zapcore.Field) {
+	l.zapLogger.Error(err.Error(), data...)
 }
 
 // Fields creates fields map for log message
-func Fields(f ...Field) map[string]interface{} {
-	fields := make(map[string]interface{})
-	for _, val := range f {
-		fields[val.Key] = val.Value
+func (l *LoggerWrapper) Fields(f ...Field) []zapcore.Field {
+	var fields []zapcore.Field
+	for _, field := range f {
+		fields = append(fields, zap.String(field.Key, field.Value))
 	}
 
 	return fields
 }
 
+var highPriority = zap.LevelEnablerFunc(func(lvl zapcore.Level) bool {
+	return lvl >= zapcore.ErrorLevel
+})
+var lowPriority = zap.LevelEnablerFunc(func(lvl zapcore.Level) bool {
+	return lvl < zapcore.ErrorLevel
+})
+
+var jsonDebugging = zapcore.AddSync(ioutil.Discard)
+var jsonErrors = zapcore.AddSync(ioutil.Discard)
+var consoleDebugging = zapcore.Lock(os.Stdout)
+var consoleErrors = zapcore.Lock(os.Stderr)
+var jsonEncoder = zapcore.NewJSONEncoder(zap.NewProductionEncoderConfig())
+var consoleEncoder = zapcore.NewConsoleEncoder(zap.NewDevelopmentEncoderConfig())
+var core = zapcore.NewTee(
+	zapcore.NewCore(jsonEncoder, jsonErrors, highPriority),
+	zapcore.NewCore(consoleEncoder, consoleErrors, highPriority),
+	zapcore.NewCore(jsonEncoder, jsonDebugging, lowPriority),
+	zapcore.NewCore(consoleEncoder, consoleDebugging, lowPriority),
+)
+
 func init() {
-	cfg := zap.NewDevelopmentConfig()
-	cfg.OutputPaths = []string{devLogOut, stderr}
-	cfg.DisableCaller = true
-	cfg.DisableStacktrace = true
-	cfg.EncoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
+	jsonDebugging := zapcore.AddSync(ioutil.Discard)
+	jsonErrors := zapcore.AddSync(ioutil.Discard)
+	consoleDebugging := zapcore.Lock(os.Stdout)
+	consoleErrors := zapcore.Lock(os.Stderr)
 
-	initialFields := make(map[string]interface{})
-	initialFields["PID"] = os.Getpid()
-	initialFields["EXE"] = path.Base(os.Args[0])
-	cfg.InitialFields = initialFields
+	jsonEncoder := zapcore.NewJSONEncoder(zap.NewProductionEncoderConfig())
+	consoleEncoder := zapcore.NewConsoleEncoder(zap.NewDevelopmentEncoderConfig())
 
-	logger, err := cfg.Build()
-	if err != nil {
-		panic(fmt.Errorf("Could not initialize fallback logger: %v", err))
+	core = zapcore.NewTee(
+		zapcore.NewCore(jsonEncoder, jsonErrors, highPriority),
+		zapcore.NewCore(consoleEncoder, consoleErrors, highPriority),
+		zapcore.NewCore(jsonEncoder, jsonDebugging, lowPriority),
+		zapcore.NewCore(consoleEncoder, consoleDebugging, lowPriority),
+	)
+
+	Logger = &LoggerWrapper{
+		zapLogger: zap.New(core),
 	}
-
-	Logger = &LoggerWrapper{logger.Sugar()}
 }
